52401fe8da7138e7d7c6669a66e7db53949fbea0
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-07-14 16:28:52 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -9,3 +9,14 @@ if time_now % 2==0
   else
   puts "Odd!"
   end
+
+
+#current_time = Time.now
+#current_time = current_time.to_i
+
+#if current_time.even?
+#puts "Even!"
+#else
+#puts "Odd!"
+
+#end
